1d82ff1c716309e234237db7042ebc49b33808ca
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2024-01-23 00:17:52 +0530

    java

---
+++ Arith.java
@@ -0,0 +1,9 @@
+public class Arith{
+	public static void main(String[] args){
+		System.out.println(2+7);
+		System.out.println(2*7);
+		System.out.println(2-7);
+		System.out.println(2/7);
+		System.out.println(2%7);
+	}
+}
